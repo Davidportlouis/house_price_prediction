35279d0b9ce9fcba534e56641c7cf1dbe5d01944
davidportlouis <dportlouis@gmail.com>
2019-09-13 08:51:00 +0530

    master push

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('./dataset/streeteasy.csv')
 # print(df.shape)
