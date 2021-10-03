6c706089fa03bc8dbac6f0f93c7a56e1735ea39e
Nalla Vedavathi <68542087+Vedavathi-nalla@users.noreply.github.com>
2021-10-03 08:14:27 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,13 +6,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 
+#Data Loading
 df = pd.read_csv('./dataset/streeteasy.csv')
 # print(df.shape)
 
-X = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs',
-        'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
-# print(X.shape)
+#Data Preprocessing
+#The features with positive correlation were extracted and made as one of the features columns The target feature was extracted from the dataframe and made as the target columns
+X = []
+#Appending the positive corr into X 
+for col in df:
+        if df['rent'].corr(df[col]) > 0 and df['rent'].corr(df[col]) < 1 :
+                X.append(col)
+
+
 y = df['rent']
+
+# print(X.shape)
+
 
 x_train, x_test, y_train, y_test = train_test_split(
     X, y, train_size=0.8, test_size=0.2, random_state=6)
